1012013b7cf0bc7d2fc5531aa62da6e11a226d97
asutermo <a131313.com>
2024-04-17 18:29:39 -0600

    remove unecessary

--- huggingface_demo.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline  # type: ignore
-
-
-
-summarizer_pipeline = pipeline("summarization", model="facebook/bart-large-cnn")
-sentiment_pipeline = pipeline("sentiment-analysis", model="siebert/sentiment-roberta-large-english")
-
-# grab links, summarize/sentiment them
